ac83aa6bedef61e0fb17baa0b606e90b08cb8b62
rionoymany-portfolio1 <rionoymany@gmail.com>
2026-01-15 10:18:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
+# Infinite loop to run the mini shell
 while True:
- cmd = input("mini-shell> ")
- if cmd == "exit" :
-  print("Bye bye see you later")
- elif cmd == "hello":
-  print("Hello,this is pentesting service")
- elif cmd == "I want to use your service":
-  print("sorry this is developing now please coming soon...")
- else:
-  print("command not found")
- break
- print(f"Executing: {cmd}")
+    # Get user input
+    cmd = input("mini-shell> ")
+
+    # Exit command
+    if cmd == "exit":
+        print("Bye bye see you later")
+        break  # Exit the loop
+
+    # Hello command
+    elif cmd == "hello":
+        print("Hello, this is pentesting service")
+
+    # Specific message command
+    elif cmd == "I want to use your service":
+        print("sorry this is developing now please coming soon...")
+
+    # Unknown command
+    else:
+        print("command not found")
+
+    # Show what command is being executed
+    print(f"Executing: {cmd}")
